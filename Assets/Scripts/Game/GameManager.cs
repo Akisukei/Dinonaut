49db5f314bde8ac28d2bc5f7815f6a3db6e4d236
Shifat Khan <shifatkhan6@gmail.com>
2020-01-11 16:05:13 -0500

    Started lava mechanics

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] lava;
+
+    private float lastHeight;
+
+    void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
