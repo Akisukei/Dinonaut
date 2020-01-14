5e3406a25762320e68ff46d082711332741e04f1
Akisukei <justin321261@hotmail.com>
2020-01-14 04:43:21 -0500

    camera now follow character regardless of initial cam position

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    //public GameObject player;
+    public Transform player;
     private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
+        Debug.Log("offseT:" + offset);
+        Debug.Log("before cam:" + transform.position);
     }
 
     // LateUpdate is called after Update in each frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+
+        //transform.position = transform.position + offset;
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        Debug.Log("cam:" + transform.position);
     }
 }
